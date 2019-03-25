1b1325a6bcd57d5e7be9ad23ae727318a345c4d0
SumanDas3001 <sumandas3001@gmail.com>
2019-03-25 18:09:15 +0530

    integrated new and create action for portfolio

--- app/controllers/own_portfolios_controller.rb
+++ app/controllers/own_portfolios_controller.rb
@@ -1,5 +1,37 @@
 class OwnPortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :update, :edit, :destroy]
+
   def index
     @portfolio_items = OwnPortfolio.all
+  end
+
+  def show
+  end
+
+  def new
+    @portfolio_item = OwnPortfolio.new
+  end
+
+  def create
+    @portfolio_item = OwnPortfolio.new(portfolio_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to own_portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_portfolio
+    @portfolio_item = OwnPortfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:own_portfolio).permit(:title, :subtitle, :body)
   end
 end
