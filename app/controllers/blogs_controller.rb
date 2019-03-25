04774b99d3d8fbf725fe9ba99032a8fbee563cfb
suman <suman@yottolabs.com>
2019-03-25 09:21:34 +0530

    changed on blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,18 +1,16 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
-  # GET /blogs
-  # GET /blogs.json
+
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
+
   def show
   end
 
-  # GET /blogs/new
+
   def new
     @blog = Blog.new
   end
