8786aba3cc2b78517cbe380bceeaecdcd565bd09
SumanDas3001 <sumandas3001@gmail.com>
2019-04-08 10:46:03 +0530

    create custom routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'own_portfolios#show', as: 'portfolio_show' # custom portfolio show path
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status # This routes it's for draft or published post
+    end
+  end
   root "pages#home"
 end
