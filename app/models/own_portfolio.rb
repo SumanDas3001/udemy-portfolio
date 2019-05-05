e29945b291287bda068a96b1ef5cc8d73e8aa177
SumanDas3001 <sumandas3001@gmail.com>
2019-05-05 20:31:05 +0530

    Implemented default image for portfolio

--- app/models/own_portfolio.rb
+++ app/models/own_portfolio.rb
@@ -1,5 +1,14 @@
 class OwnPortfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumb_image
+	validates_presence_of :title, :body, :main_image, :thumb_image
+	#### Callbacks
+	after_initialize :set_defaults  #### after_initialize only call when we create a new record.
+
+
   #### Custom scopes 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
+	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
+	
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
