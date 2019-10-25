10ed22a587fc25670943c3e960245a4a12de0192
SumanDas3001 <sumandas3001@gmail.com>
2019-10-25 18:04:17 +0530

    Implement concern for iamges

--- app/models/own_portfolio.rb
+++ app/models/own_portfolio.rb
@@ -1,4 +1,7 @@
 class OwnPortfolio < ApplicationRecord
+	###  This is a concerns used to generate image
+	include Placeholder  
+
 	validates_presence_of :title, :body, :main_image, :thumb_image
 	#### Callbacks
 	after_initialize :set_defaults  #### after_initialize only call when we create a new record.
@@ -10,7 +13,7 @@ class OwnPortfolio < ApplicationRecord
 
 	
 	def set_defaults
-		self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "http://placehold.it/350x200"
+		self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+		self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
 	end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,14 @@
 class Skill < ApplicationRecord
+ 	
+ 	###  This is a concerns used to generate image
+  include Placeholder
+  
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_defaults  #### after_initialize only call when we create a new record.
+
+	def set_defaults
+		self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+	end
 end
+
