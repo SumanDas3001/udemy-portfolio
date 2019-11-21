51d18076ada21143c1f747c9f9eea3c39ee5feaf
SumanDas3001 <sumandas3001@gmail.com>
2019-11-22 01:48:02 +0530

    Implemented persistent position updates for drag and drop interface

--- app/controllers/own_portfolios_controller.rb
+++ app/controllers/own_portfolios_controller.rb
@@ -7,6 +7,14 @@ class OwnPortfoliosController < ApplicationController
     @portfolio_items = OwnPortfolio.by_position
   end
 
+  def sort
+    params[:order].each do |key, value|
+      OwnPortfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
+  end
+
   def angular
     @angular_portfolio_items = OwnPortfolio.angular
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :own_portfolios, except: [:show]
+  resources :own_portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'own_portfolios#angular'
   get 'portfolio/:id', to: 'own_portfolios#show', as: 'portfolio_show' # custom portfolio show path
   get 'about-me', to: 'pages#about'
